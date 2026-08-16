3f6cc51c48ef60fc920108b927a6747c7d3b8a6e
Medeyros12 <medeyros12@gmail.com>
2026-08-16 17:28:07 -0300

    1st update

---
+++ scripts/test/random_element.js
@@ -0,0 +1,13 @@
+var Elements = {}
+Elements.R = [
+    id="cyan", src="/assets/test/cyan.png",
+    id="red", src="/assets/test/red.png"
+],
+
+Elements.L = [
+    id="cyan", src="/assets/test/cyan.png",
+    id="red", src="/assets/test/red.png"
+],
+
+Elements.RandomR = "/assets/test/cyan.png",
+Elements.RandomL = ~~(Math.random() * Elements.L.length)
